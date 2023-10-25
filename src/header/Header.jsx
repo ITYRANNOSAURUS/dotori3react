83244828dd418fa97b8d04ce48f0dcbbc0b29e33
ITYRANNOSAURUS <po04134@naver.com>
2023-10-25 12:21:42 +0900

    11

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './Header.module.css';
+import styles from './Header.module.css';
 
 const Header = () => {
     const [dropdownOpen1, setDropdownOpen1] = useState(false);
@@ -10,7 +10,7 @@ const Header = () => {
     return (
         <div className="p-5 my-bg-color text-white d-flex justify-content-between">
             <h1 className="my-text-size">
-                <a href="http://localhost/home"><img className="title" src="/logo.png" alt="" /></a>
+                <a href="http://localhost/home"><img className={styles.title} src="/logo.png" alt="" /></a>
             </h1>
             <ul className="navbar-nav mx-auto">
                 <li className="nav-item dropdown mr-3">
