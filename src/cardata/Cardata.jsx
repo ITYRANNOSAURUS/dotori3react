dba19926adfeb0deeb47a1b85a0221f9997ae9b0
ITYRANNOSAURUS <po04134@naver.com>
2023-10-26 14:10:30 +0900

    11

--- src/cardata/Cardata.jsx
+++ src/cardata/Cardata.jsx
@@ -16,9 +16,17 @@ const Cardata = () => {
     return (
         <div className={style['react']}>
             <p className={style['right-top-text']}>react + springboot 활용한 페이지입니다.</p>
+            <div className={style['car-data-title']}>
+                <div>
+                    <h1>전기차 주행거리</h1>
+                </div>
+                <div>
+                    <p className={style['car-data-dis']}>전기차 주행거리 : 한 번 충전으로 주행할 수 있는 거리</p>
+                </div>
+            </div>
             <div className={style['car-data-container']}>
                 <ReactPlayer
-                    url="https://www.youtube.com/watch?v=KFGES0sESy0"
+                    url="https://www.youtube.com/watch?v=nABR88G_2cE"
                     playing={true}
                     loop={true}
                     muted={true}
@@ -26,8 +34,6 @@ const Cardata = () => {
                     height="100%"
                     style={{ position: 'absolute', top: 0, left: 0, zIndex: -1 }}
                 />
-                <h1 className="car-data-heading">전기차 주행거리</h1>
-                <p className={style['car-data-distance']}>전기차 주행거리 : 한 번 충전으로 주행할 수 있는 거리</p>
                 <p className={style['car-data-intro']}>주행거리 상위 250개 전기자동차 목록입니다.<br /><br />
                     알고 싶은 차종을 검색해 보세요!</p>
                 <div className={style['search-bar']}>
@@ -42,19 +48,21 @@ const Cardata = () => {
                 </div>
             </div>
 
-            {carDatas.filter((val) => {
-                if (searchTerm === "") {
-                    return val;
-                } else if (val.model.toLowerCase().includes(searchTerm.toLowerCase())) {
-                    return val;
-                }
-            }).map((car) => (
-                <div key={car.id} className={style['car-item']}>
-                    <p>{car.model}</p>
-                    <p className={style.Km}>{car.km} km</p>
-                </div>
-            ))}
-        </div>
+            {
+                carDatas.filter((val) => {
+                    if (searchTerm === "") {
+                        return val;
+                    } else if (val.model.toLowerCase().includes(searchTerm.toLowerCase())) {
+                        return val;
+                    }
+                }).map((car) => (
+                    <div key={car.id} className={style['car-item']}>
+                        <p>{car.model}</p>
+                        <p className={style.Km}>{car.km} km</p>
+                    </div>
+                ))
+            }
+        </div >
     );
 }
 
