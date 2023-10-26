01d2bf171ecb777ae2e74e1b6a8ba230db4e34ca
bnm6283 <bnm6283@naver.com>
2023-10-26 11:14:36 +0900

    서힝

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -13,47 +13,42 @@ const Header = () => {
                 <a href="http://localhost/home"><img className={styles.title} src="/logo.png" alt="" /></a>
             </h1>
             <ul className={`navbar-nav ${styles.navbarNav}`}>
-                <li className={styles.dropdown_category}>
-                    <a className="nav-link dropdown-toggle active" href="#" id="infoDropdown" role="button"
-                        style={{ color: 'rgb(255, 179, 0)' }} onClick={() => setDropdownOpen1(!dropdownOpen1)}>충전소 찾기</a>
-                    {dropdownOpen1 && (
-                        <div className="dropdown-menu" aria-labelledby="infoDropdown">
-                            <a className='dropdown-item' href='http://localhost/map'>지도</a>
-                            <div className="dropdown">
-                                <a className='dropdown-item' href='http://localhost/post/a'>목록</a>
-                            </div>
-                        </div>
-                    )}
-                </li>
 
                 <li className={styles.dropdown_category}>
+                    <a className="nav-link dropdown-toggle active" href='http://localhost/map' id="infoDropdown" role="button"
+                        style={{ color: 'rgb(255, 179, 0)' }}>충전소 찾기</a>
+                </li>
+
+                <li className={styles.dropdown_category}
+                    onMouseEnter={() => setDropdownOpen2(true)}
+                    onMouseLeave={() => setDropdownOpen2(false)}>
                     <a className="nav-link dropdown-toggle active" href="#" id="postsDropdown" role="button"
-                        style={{ color: 'rgb(255, 179, 0)' }} onClick={() => setDropdownOpen2(!dropdownOpen2)}>게시판</a>
+                        style={{ color: 'rgb(255, 179, 0)' }}>게시판</a>
                     {dropdownOpen2 && (
-                        <div className="dropdown-menu" aria-labelledby="infoDropdown">
-                            <a className='dropdown-item' href='http://localhost/board/list'>공지사항</a>
-                            <div className="dropdown">
-                                <a className='dropdown-item' href='http://localhost/qna/list'>Q&A</a>
-                                <div className="dropdown">
-                                    <a className='dropdown-item' href='http://localhost/chlregister'>충전소 추가 요청</a>
-                                </div>
+                        <div class="dropdown-menu" aria-labelledby="infoDropdown">
+                            <a class='dropdown-item' href='http://localhost/board/list'>공지사항</a>
+                            <div class="dropdown">
+                                <a class='dropdown-item' href='http://localhost/qna/list'>Q&A</a>
                             </div>
                         </div>
+
                     )}
                 </li>
 
-                <li className={styles.dropdown_category}>
+                <li className={styles.dropdown_category}
+                    onMouseEnter={() => setDropdownOpen3(true)}
+                    onMouseLeave={() => setDropdownOpen3(false)}>
                     <a className="nav-link dropdown-toggle active" href="#" id="postsDropdown" role="button"
-                        style={{ color: 'rgb(255, 179, 0)' }} onClick={() => setDropdownOpen3(!dropdownOpen3)}>미디어</a>
+                        style={{ color: 'rgb(255, 179, 0)' }}>미디어</a>
                     {dropdownOpen3 && (
-                        <div className="dropdown-menu" aria-labelledby="infoDropdown">
-                            <a className='dropdown-item' href='http://localhost/media/news?searchText=전기차'>전기차 뉴스</a>
-                            <div className="dropdown">
-                                <a className='dropdown-item' href='http://localhost/media/reels'>전기차 영상</a>
-                                <div className="dropdown">
-                                    <a className='dropdown-item' href='/'>전기차 주행거리</a>
-                                    <div className="dropdown">
-                                        <a className='dropdown-item' href='http://localhost/media/gamepage'>전기차 게임</a>
+                        <div class="dropdown-menu" aria-labelledby="infoDropdown">
+                            <a class='dropdown-item' href='http://localhost/media/news?searchText=전기차'>전기차 뉴스</a>
+                            <div class="dropdown">
+                                <a class='dropdown-item' href='http://localhost/media/reels'>전기차 영상</a>
+                                <div class="dropdown">
+                                    <a class='dropdown-item' href='/'>전기차 주행거리</a>
+                                    <div class="dropdown">
+                                        <a class='dropdown-item' href='http://localhost/media/gamepage'>전기차 게임</a>
                                     </div>
                                 </div>
                             </div>
@@ -61,23 +56,23 @@ const Header = () => {
                     )}
                 </li>
 
-
-                <li className={styles.dropdown_category}>
+                <li className={styles.dropdown_category}
+                    onMouseEnter={() => setDropdownOpen4(true)}
+                    onMouseLeave={() => setDropdownOpen4(false)}>
                     <a className="nav-link dropdown-toggle active" href="#" id="infoDropdown" role="button"
-                        style={{ color: 'rgb(255, 179, 0)' }} onClick={() => setDropdownOpen4(!dropdownOpen4)}>멤버십</a>
+                        style={{ color: 'rgb(255, 179, 0)' }}>멤버십</a>
                     {dropdownOpen4 && (
-                        <div className="dropdown-menu" aria-labelledby="infoDropdown">
-                            <a className='dropdown-item' href='http://localhost/exchange'>코인교환</a>
-                            <div className="dropdown">
-                                <a className='dropdown-item' href='http://localhost/coupon'>쿠폰함</a>
-                                <div className="dropdown">
-                                    <a className='dropdown-item' href='http://localhost/membership'>구독하기</a>
+                        <div class="dropdown-menu" aria-labelledby="infoDropdown">
+                            <a class='dropdown-item' href='http://localhost/exchange'>코인교환</a>
+                            <div class="dropdown">
+                                <a class='dropdown-item' href='http://localhost/coupon'>쿠폰함</a>
+                                <div class="dropdown">
+                                    <a class='dropdown-item' href='http://localhost/membership'>구독하기</a>
                                 </div>
                             </div>
                         </div>
                     )}
                 </li>
-
             </ul>
         </div>
     );
